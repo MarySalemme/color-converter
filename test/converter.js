99fc4599613c01f7455e5767208ff2a6d6374c20
MarySalemme <marysalemme1@gmail.com>
2017-08-14 09:22:45 +0100

    Implementing the hex to RGB function

--- test/converter.js
+++ test/converter.js
@@ -14,9 +14,15 @@ describe("Color Converter", function() {
     });
   });
 
-  // describe("Hex to RGB conversion", function() {
-  //   it("converts the basic colours", function () {
-  //
-  //   });
-  // });
+  describe("Hex to RGB conversion", function() {
+    it("converts the basic colours", function () {
+      var red = converter.hexToRgb("ff0000");
+      var green = converter.hexToRgb("00ff00");
+      var blue = converter.hexToRgb("0000ff");
+
+      expect(red).to.deep.equal([255, 0, 0]);
+      expect(green).to.deep.equal([0,255, 0]);
+      expect(blue).to.deep.equal([0, 0, 255]);
+    });
+  });
 });
